bd366d7f0af4c20c6ed3237d0f2b15d4d0629f67
lbarberena <octaviobarberena@hotmail.com>
2020-09-28 01:48:12 -0600

    Add html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css',
+    '../assets/css/bootstrap.css',
+    '../assets/css/styles.css',
+    '../assets/css/fontawesome-all.css',
+    '../assets/css/swiper.css',
+    '../assets/css/magnific-popup.css'
+  ]
 })
 export class AppComponent {
   title = 'WEB';
